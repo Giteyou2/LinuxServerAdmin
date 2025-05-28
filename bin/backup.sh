584ce9ae73b94ae215775cdd85ef305783e96dfc
Giteyou2 <khgoh87@gmail.com>
2025-05-28 14:12:44 +0900

    first commit

--- bin/backup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /home
-tar cvzf /backup/home_$(date +%m%d_%H%M%S).tar.gz . > /backup/backup.log 2>&1
-
--- bin/userdel.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cat /root/bin/passwd.txt |while read UNAME UPASS
-do
-	userdel -r $UNAME
-
-done
